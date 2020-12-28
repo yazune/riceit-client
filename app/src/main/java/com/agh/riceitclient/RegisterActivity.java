93024770eb3084da8d9565dfe9fbdc98dccc3004
yazune <grzegorz.infs@gmail.com>
2020-12-29 00:49:46 +0100

    Create RegisterActivity with layout (part I)
    - Create RegisterActivity class for first screen of Registration functionallity
    - Create activity_register.xml for first screen of Registration
    - Add 3 icons to res/drawable and add 2 fonts to project
    - Initiate all the buttons inside the RegisterActivity class
    - Create method in RegisterActivity class which is changing activity onto another

---
+++ app/src/main/java/com/agh/riceitclient/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.agh.riceitclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView backBtn;
+    Button nextBtn, loginBtn;
+    TextView titleText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Hooks
+        backBtn = findViewById(R.id.register_back_button);
+        nextBtn = findViewById(R.id.register_next_button);
+        loginBtn = findViewById(R.id.register_login_button);
+        titleText = findViewById(R.id.register_title_text);
+    }
+
+    public void callNextRegisterScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(), RegisterUserDetailsActivity.class);
+
+        //Add Transition
+        //todo - check if Pair should be a basic one or androixX
+        Pair[] pairs = new Pair[4];
+        pairs[0] = new Pair<View, String>(backBtn, "transition_back_arrow_btn");
+        pairs[1] = new Pair<View, String>(backBtn, "transition_title_text");
+        pairs[2] = new Pair<View, String>(backBtn, "transition_next_btn");
+        pairs[3] = new Pair<View, String>(backBtn, "transition_login_btn");
+
+        //todo - it should be handled by if/else with checking Build.Version_Codes.LOLLIPOP
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(RegisterActivity.this, pairs);
+        startActivity(intent, activityOptions.toBundle());
+    }
+}
