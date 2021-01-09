57eeb07b414f689921734360b69aafa6aa527feb
yazune <grzegorz.infs@gmail.com>
2021-01-09 11:53:48 +0100

    Create beginning activity
    - Create WelcomeScreenActivity class
    - Create activity_welcome_screen layout file
    - Add icon_circle_r.png for activity_welcome_screen
    - Create 2 buttons in WelcomeScreenActivity which open LoginActivity or RegisterActivity
    - Change MAIN/LAUNCHER activity in AndroidManifest.xml for WelcomeScreenActivity
    - Add INTERNET permission in AndroidManifest.xml

---
+++ app/src/main/java/com/agh/riceitclient/activity/WelcomeScreenActivity.java
@@ -0,0 +1,50 @@
+package com.agh.riceitclient.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+
+import com.agh.riceitclient.R;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+    Button loginBtn, registerBtn, additionalBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_welcome_screen);
+
+        // Hooks
+        loginBtn = findViewById(R.id.welcome_login_button);
+        registerBtn = findViewById(R.id.welcome_register_button);
+        additionalBtn = findViewById(R.id.welcome_additional_button);
+    }
+
+    public void callLoginScreen(View view){
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.welcome_login_button),"transition_login");
+
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(WelcomeScreenActivity.this, pairs);
+        startActivity(intent, activityOptions.toBundle());
+    }
+
+    public void callRegisterScreen(View view){
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.welcome_register_button),"transition_register");
+
+        ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(WelcomeScreenActivity.this, pairs);
+        startActivity(intent, activityOptions.toBundle());
+    }
+}
